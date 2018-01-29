dadd292807d416a7c3b541a318b94693e5201243
HypeUFO <michael.maio22@gmail.com>
2018-01-28 16:24:44 -0800

    cleanup

--- src/js/components/Timpicker.js
+++ src/js/components/Timpicker.js
@@ -1,40 +1,9 @@
-// basic usage
-// in some react component
 import React from "react";
 import TimePicker from "react-times";
 
-// use material theme
-// import "react-times/css/material/default.css";
-// or you can use classic theme
 import "react-times/css/classic/default.css";
 
 export default class SomeComponent extends React.Component {
-  // do some work
-  onHourChange(hour) {
-    // do something
-    console.log("on hour change");
-  }
-
-  onMinuteChange(minute) {
-    // do something
-    console.log("on minute change");
-  }
-
-  onTimeChange(time) {
-    // do something
-    console.log("on time change");
-  }
-
-  onFocusChange(focusStatue) {
-    // do something
-    console.log("on focus change");
-  }
-
-  onMeridiemChange(meridiem) {
-    // do something
-    console.log("on meridiem change");
-  }
-
   render() {
     const {
       name,
